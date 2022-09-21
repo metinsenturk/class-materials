3a4f621a2f037d3ebf5e2f3a0fba6323f6cf6c01
Metin Senturk <metinsenturk@me.com>
2022-09-20 22:22:16 -0400

    add comments for map reduce

--- contents/map-reduce/simple.py
+++ contents/map-reduce/simple.py
@@ -4,15 +4,15 @@ from itertools import groupby
 # ========= Mapping ==========
 words = ['Deer', 'Bear', 'River', 'Car',
          'Car', 'River', 'Deer', 'Car', 'Bear']
-mapping = map((lambda x: (x, 1)), words)
-print(mapping)
+mapping = list(map((lambda x: (x, 1)), words))
+print('Mapping:', mapping)
 # output:
 # [('Deer', 1), ('Bear', 1), ('River', 1), ('Car', 1),
 # ('Car', 1), ('River', 1), ('Deer', 1), ('Car', 1), ('Bear', 1)]
 
 # ========= Shuffling ==========
 sorted_mapping = sorted(mapping)
-print(sorted_mapping)
+print('Shuffling:', sorted_mapping)
 # output:
 # [('Bear', 1), ('Bear', 1), ('Car', 1), ('Car', 1),
 # ('Car', 1), ('Deer', 1), ('Deer', 1), ('River', 1), ('River', 1)]
@@ -21,6 +21,6 @@ print(sorted_mapping)
 grouper = groupby(sorted_mapping, lambda p: p[0])
 final = map(lambda l: (l[0], reduce(lambda x, y: x +
                                     y, map(lambda p: p[1], l[1]))), grouper)
-print(list(final))
+print('Reducing:', list(final))
 # output:
 # [('Bear', 2), ('Car', 3), ('Deer', 2), ('River', 2)]
